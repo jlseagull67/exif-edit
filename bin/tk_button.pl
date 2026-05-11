99694f165ab6f3f1b5fb187f8b41d0235c34983b
Jörg Albrecht <j.l.seagull@gmx.de>
2026-05-11 14:02:50 +0200

    perl-tk samples removed

--- bin/tk_button.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl -w
-use Tk;
-use strict;
-my $main = new MainWindow;
-my $button = $main->Button();
-$button -> configure(-text => 'Press me!');
-$button -> pack;
-MainLoop;
--- bin/tk_demo.pl
+++
@@ -1,9 +0,0 @@
-use WidgetDemo;
- my $TOP = $MW->WidgetDemo(
-     -name             => $demo,
-     -text             => 'Learn how to write a widget demonstration!',
-     -title            => 'WidgetDemo Demonstration',
-     -iconname         => 'WidgetDemo',
-     -geometry_manager => 'grid',
-     -font             => $FONT,
- );
